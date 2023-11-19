de7ed8f3f8748ffb9c951881de93a4ad9459b9fd
Eamonn O'Neill <eamonn.oneill@ryerson.ca>
2023-11-18 20:12:46 -0500

    Created navbar skelton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,20 @@ import Info from "./screens/Info";
 import Landing from "./screens/Landing";
 import Recipes from "./screens/Recipes";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import TodaysRecipe from "./screens/TodaysRecipe";
 function App() {
   return (
     <Router>
       <div className="App">
+      <nav>
+          <Link to ="/" >Home</Link>
+          <Link to ="/todays-recipe" >Today's Recipe</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/recipe/:term" element={<Recipes />} />
           <Route path="/info/:term" element={<Info />} />
+          <Route path="/todays-recipe" element={<TodaysRecipe></TodaysRecipe>}></Route>
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/screens/TodaysRecipe.js
@@ -0,0 +1,8 @@
+function TodaysRecipe(){
+
+    return(
+        <div>
+            <h1>Today's Recipe</h1>
+        </div>)
+}
+export default TodaysRecipe

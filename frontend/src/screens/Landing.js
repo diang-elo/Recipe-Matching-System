8c8f5cadd640852c951916b6774c0da8e9521357
diangelo <diangelodasilva@hotmail.com>
2023-11-18 19:10:26 -0500

    search by options in input

--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -5,6 +5,7 @@ import RecipeCard from "../components/cards/RecipeCard";
 function Landing() {
   const navigate = useNavigate();
   const [searchName, setSearchName] = useState("");
+  const [searchType, setSearchType] = useState("");
   const getSearch = (e) => {
     e.preventDefault();
     navigate("/recipe/" + searchName);
@@ -18,46 +19,42 @@ function Landing() {
         </h1>
         <form class="sm:w-3/5 w-80">
           <label
-            for="default-search"
+            for="search-type"
             class="mb-2 text-sm font-medium text-gray-900 sr-only"
           >
             Search
           </label>
-          <div class="relative">
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg
-                class="w-5 h-5 text-gray-500 "
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </div>
+          <div class="relative flex">
+            <select
+              id="search-type"
+              onChange={(event) => {
+                setSearchType(event.target.value);
+              }}
+              class="block p-4 pr-8 text-sm text-gray-900 bg-gray-50 rounded-l-lg border border-gray-300 focus:ring-blue-500"
+            >
+              <option value="ingredients">Search by Ingredients</option>
+              <option value="diet">Search by Diet</option>
+              <option value="recipe">Search by Recipe</option>
+            </select>
             <input
               onChange={(event) => {
                 setSearchName(event.target.value);
               }}
               type="search"
               id="default-search"
-              class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 "
+              class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50  border border-gray-300 focus:ring-blue-500"
               placeholder="Search Ingredient"
             />
             <button
               onClick={getSearch}
               type="submit"
-              class="text-white absolute right-2.5 bottom-2.5 bg-lime-800 hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-lime-900 font-medium rounded-lg text-sm px-4 py-2"
+              class="text-white bg-lime-800 hover:bg-lime-900 focus:ring-4 focus:outline-none focus:ring-lime-900 font-medium rounded-r-lg text-sm px-4 py-2"
             >
               Search
             </button>
           </div>
         </form>
+
         <div class="flex justify-center">
           <RecipeCard
             title="Mocha Latte"
